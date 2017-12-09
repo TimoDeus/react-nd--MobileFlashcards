1a67e83e7ae6c48c9bfc7c854900e7a22ca8058b
Timo Deuschle <github@timodeuschle.de>
2017-12-09 13:24:15 +0100

    added form to add new deck

--- src/components/AddDeck.js
+++ src/components/AddDeck.js
@@ -1,20 +1,42 @@
-import React from 'react';
-import {Container, Content, Text} from 'native-base';
+import React, {Component} from 'react';
+import {Button, Container, Content, Form, Input, Item, Label, Text} from 'native-base';
 import DefaultHeader from './header/DefaultHeader';
 import PropTypes from 'prop-types';
 import BackButton from './header/BackButton';
 
-const AddDeck = props => {
-	const {navigation} = props;
-	return (
-		<Container>
-			<DefaultHeader title='Add Deck' left={<BackButton navigation={navigation}/>}/>
-			<Content>
-				<Text>AddDeck</Text>
-			</Content>
-		</Container>
-	);
-};
+class AddDeck extends Component {
+
+	constructor() {
+		super();
+		this.state = {name: ''}
+	}
+
+	submit = () => {
+		const {navigation} = this.props;
+		navigation.state.params.onSubmitHandler(this.state.name).then(
+			() => navigation.goBack()
+		);
+	};
+
+	render() {
+		const {navigation} = this.props;
+		return (
+			<Container>
+				<DefaultHeader title='Add deck' left={<BackButton navigation={navigation}/>}/>
+				<Content>
+					<Form>
+						<Item floatingLabel>
+							<Label>Deck name</Label>
+							<Input onChangeText={name => this.setState({name})} />
+						</Item>
+					</Form>
+					<Button onPress={this.submit}><Text>Add deck</Text></Button>
+					<Text>{this.state.name}</Text>
+				</Content>
+			</Container>
+		);
+	}
+}
 
 AddDeck.propTypes = {
 	navigation: PropTypes.shape().isRequired
--- src/components/DeckList.js
+++ src/components/DeckList.js
@@ -24,42 +24,37 @@ export default class DeckList extends Component {
 		);
 	};
 
-	addNewDeck = () => {
-		const name = 'myDeck';
-		addDeck(name).then(this.fetchAllDecks);
+	addNewDeck = name => {
+		return addDeck(name).then(this.fetchAllDecks);
 	};
 
 	addNewCard = () => {
 		const card = {name: Date.now()};
 		const existingCards = this.state.decks['myDeck'];
 		const cards = [...existingCards, card];
-		updateDeck('myDeck', cards).then(this.fetchAllDecks);
+		return updateDeck('myDeck', cards).then(this.fetchAllDecks);
 	};
 
 	removeDecks = () => {
-		clearDecks().then(this.fetchAllDecks);
+		return clearDecks().then(this.fetchAllDecks);
 	};
 
 	render() {
 		return (
 			<Container>
-				<DefaultHeader title='Mobile Flashcards'/>
+				<DefaultHeader title='Choose your deck'/>
 				<Content>
 					<Text>{JSON.stringify(this.state.decks)}</Text>
-					<Button onPress={() => this.addNewDeck()}>
-						<Text>Add New Deck</Text>
-					</Button>
 					<Button onPress={() => this.addNewCard()}>
 						<Text>Add New Card</Text>
 					</Button>
 					<Button onPress={() => this.removeDecks()}>
 						<Text>Remove all decks</Text>
 					</Button>
-
 				</Content>
 				<Fab
 					position="bottomRight"
-					onPress={() => this.props.navigation.navigate(ADD_DECK_VIEW)}>
+					onPress={() => this.props.navigation.navigate(ADD_DECK_VIEW, {onSubmitHandler: this.addNewDeck})}>
 					<Icon name="add"/>
 				</Fab>
 			</Container>
